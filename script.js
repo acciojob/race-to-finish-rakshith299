f600e8fe1abe9fbbd91d49a70ccc5d85334f59dc
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-22 12:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+for(let i = 0; i < 5; i++){
+
+    const max = 5;
+    const min = 1;
+    let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+
+    promises.push(new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, randomNumber)
+    }))
+
+}
+
+const div = document.getElementById("output");
+
+Promise.any(promises)
+
+.then((result) => {   
+    div.innerText = "Atleast one Promise is fulfilled";
+})
+
+.catch((result) => {
+    div.innerText = "All Promises are rejected";
+})
